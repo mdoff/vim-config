806654d2c2d04c80f8313d1d9555fe7eb4d4abbd
Tomasz Lewinski <mdoff@mdoff.net>
2023-09-04 22:08:01 +0200

    redo

--- init.lua
+++ init.lua
@@ -134,7 +134,6 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    --    'navarasu/onedark.nvim',
     'joshdick/onedark.vim',
     priority = 1000,
     config = function()
@@ -148,7 +147,7 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'onedark',
         component_separators = '|',
         section_separators = '',
@@ -215,7 +214,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   {
     'MunifTanjim/prettier.nvim',
     dependencies = { 'jose-elias-alvarez/null-ls.nvim' }
@@ -538,3 +537,5 @@ cmp.setup {
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 vim.wo.relativenumber = true
+vim.opt.spelllang = 'en_gb'
+vim.opt.spell = true
