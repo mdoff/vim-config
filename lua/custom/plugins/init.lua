c33f1b1e10fe6960979aad9e2fdc9586bd3cc0b9
Tomasz Lewinski <mdoff@mdoff.net>
2023-10-18 22:18:48 +0200

    additional mappings

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,4 +9,10 @@ vim.opt.spell = true
 vim.keymap.set('n', '<leader>b', function() vim.cmd(':b#') end, { desc = 'Go to previous buffer' })
 vim.keymap.set('n', '<leader>sc', function() vim.cmd("Telescope git_status") end,
   { desc = 'Search files changed since last commit' })
+vim.keymap.set('n', '<leader>td', function() vim.cmd('Gitsigns toggle_deleted') end,
+  { desc = '[T]oggle showing [D]eleted hunks' })
+vim.keymap.set('n', '<leader>tb', function() vim.cmd('GitBlameToggle') end,
+  { desc = '[T]oggle showing current line [B]lame' })
+vim.keymap.set('n', '<c-d>', '<c-d>zz', { noremap = false })
+vim.keymap.set('n', '<c-u>', '<c-u>zz', { noremap = false })
 return {}
