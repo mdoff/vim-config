d0e1b35b9cc196289d43dc884128bf9c0c6f2a3c
Tomasz Lewinski <mdoff@mdoff.net>
2023-10-18 22:29:41 +0200

    more keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,11 +8,17 @@ vim.opt.spell = true
 
 vim.keymap.set('n', '<leader>b', function() vim.cmd(':b#') end, { desc = 'Go to previous buffer' })
 vim.keymap.set('n', '<leader>sc', function() vim.cmd("Telescope git_status") end,
-  { desc = 'Search files changed since last commit' })
+  { desc = '[S]earch files [C]hanged since last commit' })
 vim.keymap.set('n', '<leader>td', function() vim.cmd('Gitsigns toggle_deleted') end,
   { desc = '[T]oggle showing [D]eleted hunks' })
 vim.keymap.set('n', '<leader>tb', function() vim.cmd('GitBlameToggle') end,
   { desc = '[T]oggle showing current line [B]lame' })
 vim.keymap.set('n', '<c-d>', '<c-d>zz', { noremap = false })
 vim.keymap.set('n', '<c-u>', '<c-u>zz', { noremap = false })
+vim.keymap.set('n', '<leader>gp', function() vim.cmd("Gitsigns prev_hunk") end,
+  { desc = '[G]o to [P]revious git hunk' })
+vim.keymap.set('n', '<leader>gn', function() vim.cmd("Gitsigns next_hunk") end,
+  { desc = '[G]o to [N]ext git hunk' })
+vim.keymap.set('n', '<leader>tp', function() vim.cmd('Gitsigns preview_hunk') end,
+  { desc = '[T]oggle git [P]review' })
 return {}
