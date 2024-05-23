b0b83ddb348eb698e1daaa4f2bdb07192996f049
Tomasz Lewinski <mdoff@mdoff.net>
2024-05-23 11:13:58 +0200

    Squashed commit of the following:
    commit 1d949ee0734ddb2280841a3eb04a24f8a953a71c
    Author: Tomasz Lewinski <mdoff@mdoff.net>
    Date:   Thu May 23 11:13:09 2024 +0200
    
        Add oil.nvim plugin
    
    commit 9d8cd1b33a70946d5ecdd8461d7c305eb97182ed
    Author: Tomasz Lewinski <tomasz.lewinski@scalac.io>
    Date:   Sat May 18 22:25:30 2024 +0200
    
        better copilot
    
    commit c487e1ae7193e2faed3998c6c46d0ad9137ee874
    Author: Tomasz Lewinski <mdoff@mdoff.net>
    Date:   Mon Apr 15 16:03:28 2024 +0200
    
        update config
    
    commit 61f777fe7999ba09aa04598ef52f1a4452eaa712
    Author: Tomasz Lewinski <tomasz.lewinski@scalac.io>
    Date:   Tue Apr 9 08:32:45 2024 +0200
    
        I can live with noice.lua
    
    commit 6741f86482e3e5915265dc87095e1782cf054271
    Author: Tomasz Lewinski <mdoff@mdoff.net>
    Date:   Fri Mar 22 08:00:51 2024 +0100
    
        Disable noice plugin, add go format to conform plugin
    
    commit 38c7d5be4b29aaf7025c2b9ad076b8ce6337f177
    Author: Tomasz Lewinski <tomasz.lewinski@scalac.io>
    Date:   Thu Mar 21 10:39:45 2024 +0100
    
        new plugins and some config
    
    commit 67a404fb812b3999fad519daba2cfb27ec6c2e41
    Author: Tomasz Lewinski <mdoff@mdoff.net>
    Date:   Thu Mar 21 09:46:59 2024 +0100
    
        New neovim kickstart template

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,22 +3,52 @@
 --
 -- See the kickstart.nvim README for more information
 vim.wo.relativenumber = true
-vim.opt.spelllang = 'en_gb'
+vim.opt.spelllang = "en_gb"
 vim.opt.spell = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
-vim.keymap.set('n', '<leader>b', function() vim.cmd(':b#') end, { desc = 'Go to previous buffer' })
-vim.keymap.set('n', '<leader>sc', function() vim.cmd("Telescope git_status") end,
-  { desc = '[S]earch files [C]hanged since last commit' })
-vim.keymap.set('n', '<leader>td', function() vim.cmd('Gitsigns toggle_deleted') end,
-  { desc = '[T]oggle showing [D]eleted hunks' })
-vim.keymap.set('n', '<leader>tb', function() vim.cmd('GitBlameToggle') end,
-  { desc = '[T]oggle showing current line [B]lame' })
-vim.keymap.set('n', '<c-d>', '<c-d>zz', { noremap = false })
-vim.keymap.set('n', '<c-u>', '<c-u>zz', { noremap = false })
-vim.keymap.set('n', '<leader>hP', function() vim.cmd("Gitsigns prev_hunk") end,
-  { desc = 'go to preview git hunk' })
-vim.keymap.set('n', '<leader>hn', function() vim.cmd("Gitsigns next_hunk") end,
-  { desc = 'go to next git hunk' })
+vim.keymap.set("n", "<leader>b", function()
+	vim.cmd(":b#")
+end, { desc = "Go to last visited buffer" })
+vim.keymap.set("n", "<leader>p", function()
+	vim.cmd(":bp")
+end, { desc = "Go to previous buffer" })
+vim.keymap.set("n", "<leader>n", function()
+	vim.cmd(":bn")
+end, { desc = "Go to next buffer" })
+vim.keymap.set("n", "<leader>sc", function()
+	vim.cmd("Telescope git_status")
+end, { desc = "[S]earch files [C]hanged since last commit" })
+vim.keymap.set("n", "<leader>td", function()
+	vim.cmd("Gitsigns toggle_deleted")
+end, { desc = "[T]oggle showing [D]eleted hunks" })
+vim.keymap.set("n", "<leader>tb", function()
+	vim.cmd("GitBlameToggle")
+end, { desc = "[T]oggle showing current line [B]lame" })
+vim.keymap.set("n", "<c-d>", "<c-d>zz", { noremap = false })
+vim.keymap.set("n", "<c-u>", "<c-u>zz", { noremap = false })
+
+vim.keymap.set("n", "<leader>hn", function()
+	vim.cmd("Gitsigns next_hunk")
+end, { desc = "go to next git hunk" })
+
+vim.keymap.set("n", "<leader>hP", function()
+	vim.cmd("Gitsigns prev_hunk")
+end, { desc = "go to previous git hunk" })
+
+vim.keymap.set("n", "<leader>hp", function()
+	vim.cmd("Gitsigns preview_hunk")
+end, { desc = "preview git hunk" })
+
+vim.keymap.set("n", "<leader>hr", function()
+	vim.cmd("Gitsigns reset_hunk")
+end, { desc = "reset git hunk" })
+
+vim.keymap.set("n", "<leader>a", function()
+	vim.cmd("Neogit kind=replace")
+end, { desc = "Open NeoGit in floating window" })
+
 -- vim.keymap.set('n', '<leader>tp', function() vim.cmd('Gitsigns preview_hunk') end,
 --   { desc = '[T]oggle git [P]review' })
 -- vim.keymap.set('n', '<leader>rh', function() vim.cmd("Gitsigns reset_hunk") end,
--- lua/custom/plugins/nvim-ts-autotag.lua
+++ lua/custom/plugins/nvim-ts-autotag.lua
@@ -1,11 +1,3 @@
 return {
-  "windwp/nvim-ts-autotag",
-  version = "*",
-  config = function()
-    require 'nvim-treesitter.configs'.setup {
-      autotag = {
-        enable = true,
-      }
-    }
-  end
+	"windwp/nvim-ts-autotag",
 }
