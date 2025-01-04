2337447d6649625da5f7d87bec70ec21ed860b3a
Tomasz Lewinski <tomasz.lewinski@scalac.io>
2025-01-04 20:37:26 +0100

    enable copilot, add snacks, disable supermaven

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,8 +3,8 @@
 -- vim.api.nvim_set_keymap("i", "<C-H>", "copilot#Previous()", { silent = true, expr = true })
 -- vim.api.nvim_set_keymap("i", "<C-K>", "copilot#Next()", { silent = true, expr = true })
 
--- return {
--- 	"github/copilot.vim",
--- 	version = "*",
--- }
-return {}
+return {
+	"github/copilot.vim",
+	version = "*",
+}
+-- return {}
