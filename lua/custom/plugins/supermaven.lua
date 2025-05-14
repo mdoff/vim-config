b870a4d57ec15f8af9d9888c97fa99ee5bb63a2b
Tomasz Lewinski <mdoff@laptok.local>
2025-05-14 22:10:45 +0200

    Remove not used plugins

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,7 +1,6 @@
-return {}
--- return {
--- 	"supermaven-inc/supermaven-nvim",
--- 	config = function()
--- 		require("supermaven-nvim").setup({})
--- 	end,
--- }
+return {
+	"supermaven-inc/supermaven-nvim",
+	config = function()
+		require("supermaven-nvim").setup({})
+	end,
+}
